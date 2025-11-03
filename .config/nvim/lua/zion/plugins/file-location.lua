10ab85f0841508117b2a3a93a3fe0b7c5b6365ca
xuzeyang <xuzeyang@bytedance.com>
2025-11-03 13:55:35 +1100

    Add copy path shortcuts

--- .config/nvim/lua/zion/plugins/file-location.lua
+++ .config/nvim/lua/zion/plugins/file-location.lua
@@ -3,11 +3,25 @@ return {
 	config = function()
 		local fileLocation = require("nvim-file-location")
 		fileLocation.setup({
-			keymap = "<leader>L",
+			keymap = "<leader>ll",
 			mode = "workdir", -- options: workdir | absolute
 			add_line = true,
 			add_column = false,
 			default_register = "*",
 		})
+
+		local keymap = vim.keymap
+		keymap.set(
+			"n",
+			"<leader>lf",
+			"<cmd>lua NvimFileLocation.copy_file_location('absolute', true, false)<cr>",
+			{ desc = "Copy absolute file path" }
+		)
+		keymap.set(
+			"n",
+			"<leader>lF",
+			"<cmd>lua NvimFileLocation.copy_file_location('workdir', true, false)<cr>",
+			{ desc = "Copy working directory file path" }
+		)
 	end,
 }
