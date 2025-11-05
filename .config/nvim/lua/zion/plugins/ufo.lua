f8099cdfd9bbd5da2b55eeb37f66e128c46489ec
xuzeyang <xuzeyang@bytedance.com>
2025-11-05 18:39:50 +1100

    optimize fold

--- .config/nvim/lua/zion/plugins/ufo.lua
+++ .config/nvim/lua/zion/plugins/ufo.lua
@@ -13,8 +13,27 @@ return {
 		opt.foldlevelstart = 99
 		opt.foldenable = true
 
-		vim.keymap.set("n", "zR", ufo.openAllFolds)
-		vim.keymap.set("n", "zM", ufo.closeAllFolds)
+		-- Track current fold level
+		local fold_level = 0
+
+		vim.keymap.set("n", "zR", function()
+			ufo.openAllFolds()
+		end)
+		vim.keymap.set("n", "zM", function()
+			ufo.closeAllFolds()
+			fold_level = 0
+		end)
+		vim.keymap.set("n", "=", function()
+			fold_level = fold_level + 1
+			ufo.closeFoldsWith(fold_level)
+		end)
+		vim.keymap.set("n", "-", function()
+			fold_level = math.max(0, fold_level - 1)
+			ufo.closeFoldsWith(fold_level)
+		end)
+		vim.keymap.set("n", "zr", function()
+			ufo.openFoldsExceptKinds({ "imports", "comment" })
+		end, { desc = "Open folds except imports/comments" })
 
 		ufo.setup({
 			provider_selector = function(bufnr, filetype, buftype)
